ecf4deab99b4d9b5440fca31a4497d4fb6b8cf51
333fps <ronan.fesselier@gmail.com>
2024-08-21 23:08:49 +0200

    OpengGL Debug Callback improvement
    * Changes:
        - The OpengGL Debugger class is now static to allow smooth and safe insertion in any part of the code at runtime.
    
        - Removed preprocessor macros SCION_OPENGL_DEBUG_BREAKONERROR & SCION_OPENGL_DEBUG_BREAKONWARNING from from CMakeLists.txt
        and replaced by OpenGLDebugger::breakOnError(bool) & OpenGLDebugger::breakOnWarning(bool) to save compile time when option changed
        (the whole SCION libs are statics).
    
        - Added methods OpenGLDebugger::push(id) & OpenGLDebugger::pop(id) to enable or disable warnings on the fly in any part of the code.
    
        - Added method OpenGLDebugger::setSeverityLevel(OpenGLDebuggerSeverity::Disable/Notification/Low/Medium/High).
    
        - Added possibility to send a list of Ids to ignore at runtime through the OpenGLDebugger::init(std::vector<unsigned>) method.
    
    * Other:
        - Self documented code
        - Updated README.md

--- SCION_RENDERING/include/Rendering/Utils/OpenGLDebugger.h
+++ SCION_RENDERING/include/Rendering/Utils/OpenGLDebugger.h
@@ -1,21 +1,64 @@
 #pragma once
 
+#include <vector>
+
 namespace SCION_RENDERING
 {
+enum class OpenGLDebuggerSeverity : short
+{
+	Disable = 0,
+	Notification,
+	Low,
+	Medium,
+	High
+};
+
 class OpenGLDebugger final
 {
-  public:
-	OpenGLDebugger();
+  private:
+	OpenGLDebugger() = delete;
 	OpenGLDebugger( const OpenGLDebugger& ) = delete;
 	OpenGLDebugger& operator=( const OpenGLDebugger& ) = delete;
 	OpenGLDebugger( OpenGLDebugger&& ) = delete;
 	OpenGLDebugger& operator=( OpenGLDebugger&& ) = delete;
-	~OpenGLDebugger();
+	~OpenGLDebugger() = delete;
 
   public:
 	/*
 	 * @brief Initializes the OpenGL debug callback. A valid OpenGL context is necessary.
 	 */
-	void init();
+	static void init();
+
+	/*
+	 * @brief Initializes the OpenGL debug callback. A valid OpenGL context is necessary.
+	 * @param The list of Ids you want to de-activate by default.
+	 */
+	static void init( const std::vector<unsigned int>& p_ignoreList );
+
+	/*
+	 * @brief Pushes an OpenGL error Id to the ignore list.
+	 * @param The error Id given by the error callback you want to de-activate.
+	 */
+	static void push( unsigned int p_errorId );
+
+	/*
+	 * @brief Removes an OpenGL error Id from the ignore list.
+	 * @param The error Id given by the error callback to re-activate.
+	 */
+	static void pop( unsigned int p_errorId );
+
+	/*
+	 * @brief Clears the ignore list.
+	 */
+	static void clear();
+
+	/*
+	 * @brief Retrieves the ignore list.
+	 */
+	static std::vector<unsigned int> getIgnoreList();
+
+	static void breakOnError( bool );
+	static void breakOnWarning( bool );
+	static void setSeverityLevel( OpenGLDebuggerSeverity );
 };
 } // namespace SCION_RENDERING
--- cmake/Options.cmake
+++ cmake/Options.cmake
@@ -5,20 +5,15 @@
 #--------------- Preprocessor definitions -------------------#
 if(SCION_OPENGL_DEBUG_CALLBACK)
 	add_compile_definitions(SCION_OPENGL_DEBUG_CALLBACK)
+	message ( "Scion engine will be built with embedded realtime OpenGL debugger." )
 endif()
 
-if(SCION_OPENGL_DEBUG_CALLBACK_FWD_COMPATIBILITY)
-	add_compile_definitions(SCION_OPENGL_DEBUG_CALLBACK_FWD_COMPATIBILITY)
-endif()
-
-if(SCION_OPENGL_DEBUG_BREAKONERROR)
-	add_compile_definitions(SCION_OPENGL_DEBUG_BREAKONERROR)
-endif()
-
-if(SCION_OPENGL_DEBUG_BREAKONWARNING)
-	add_compile_definitions(SCION_OPENGL_DEBUG_BREAKONWARNING)
+if(SCION_OPENGL_DEBUG_FORWARD_COMPATIBILITY)
+	add_compile_definitions(SCION_OPENGL_DEBUG_FORWARD_COMPATIBILITY)
+	message ( "Scion engine will be build with a strict OpenGL context. This will trigger warnings on deprecated OpenGL dunctions." )
 endif()
 
 if(SCION_OPENGL_FORCE_DISCRETE_GPU)
 	add_compile_definitions(SCION_OPENGL_FORCE_DISCRETE_GPU)
+	message ( "Scion engine will attempt to force discrete GPU on optimus laptops." )
 endif()
