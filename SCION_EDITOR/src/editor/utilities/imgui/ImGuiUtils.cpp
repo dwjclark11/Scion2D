1eb494522c16d3645761bbc3b1d99adcbb19c279
Dustin Clark <dwjclark11@gmail.com>
2025-06-16 09:16:34 -0400

    Update ImGui Styles
    * Adjusted to be more clean looking.
    * Used a darker mode style.

--- SCION_EDITOR/src/editor/utilities/imgui/ImGuiUtils.cpp
+++ SCION_EDITOR/src/editor/utilities/imgui/ImGuiUtils.cpp
@@ -6,31 +6,58 @@ namespace ImGui
 
 void InitDefaultStyles()
 {
-	// TODO: Add this to a custom init file that we can change/adjust easier.
-
-	// Get the styles
 	ImGuiStyle& style = ImGui::GetStyle();
+	ImVec4* colors = style.Colors;
 
-	style.TabRounding = 4.f;
-	style.TabBorderSize = 1.f;
-	style.FrameBorderSize = 0.1f;
-	style.FramePadding.x = 4.f;
-	style.FramePadding.y = 3.f;
-	style.FrameRounding = 2.f;
-	style.ScrollbarSize = 10.f;
-	style.ScrollbarRounding = 6.f;
-	style.WindowBorderSize = 1.f;
-	style.WindowPadding.x = 10.f;
-	style.WindowPadding.y = 10.f;
-	style.WindowRounding = 8.f;
-	style.WindowTitleAlign.x = 0.5f;
-	style.ItemSpacing.x = 3.f;
-	style.ItemInnerSpacing.x = 0.f;
-	style.ItemInnerSpacing.y = 4.f;
-	style.ButtonTextAlign.x = 0.5f;
-	style.ButtonTextAlign.y = 0.5f;
+	// Base dark theme
+	colors[ ImGuiCol_Text ] = ImVec4( 0.86f, 0.86f, 0.86f, 1.00f );
+	colors[ ImGuiCol_WindowBg ] = ImVec4( 0.13f, 0.14f, 0.17f, 1.00f );
+	colors[ ImGuiCol_ChildBg ] = ImVec4( 0.10f, 0.11f, 0.13f, 1.00f );
+	colors[ ImGuiCol_PopupBg ] = ImVec4( 0.12f, 0.12f, 0.14f, 1.00f );
+	colors[ ImGuiCol_Border ] = ImVec4( 0.43f, 0.43f, 0.50f, 0.50f );
+	colors[ ImGuiCol_FrameBg ] = ImVec4( 0.20f, 0.21f, 0.24f, 1.00f );
+	colors[ ImGuiCol_FrameBgHovered ] = ImVec4( 0.28f, 0.30f, 0.33f, 1.00f );
+	colors[ ImGuiCol_FrameBgActive ] = ImVec4( 0.33f, 0.36f, 0.39f, 1.00f );
+	colors[ ImGuiCol_TitleBg ] = ImVec4( 0.10f, 0.11f, 0.13f, 1.00f );
+	colors[ ImGuiCol_TitleBgActive ] = ImVec4( 0.12f, 0.14f, 0.16f, 1.00f );
+	colors[ ImGuiCol_TitleBgCollapsed ] = ImVec4( 0.08f, 0.09f, 0.10f, 1.00f );
+	colors[ ImGuiCol_MenuBarBg ] = ImVec4( 0.14f, 0.14f, 0.17f, 1.00f );
+	colors[ ImGuiCol_ScrollbarBg ] = ImVec4( 0.05f, 0.05f, 0.10f, 0.53f );
+	colors[ ImGuiCol_ScrollbarGrab ] = ImVec4( 0.21f, 0.22f, 0.23f, 1.00f );
+	colors[ ImGuiCol_ScrollbarGrabHovered ] = ImVec4( 0.31f, 0.32f, 0.33f, 1.00f );
+	colors[ ImGuiCol_ScrollbarGrabActive ] = ImVec4( 0.41f, 0.43f, 0.44f, 1.00f );
+	colors[ ImGuiCol_CheckMark ] = ImVec4( 0.21f, 0.49f, 0.76f, 1.00f );
+	colors[ ImGuiCol_SliderGrab ] = ImVec4( 0.21f, 0.49f, 0.76f, 1.00f );
+	colors[ ImGuiCol_SliderGrabActive ] = ImVec4( 0.21f, 0.56f, 0.86f, 1.00f );
+	colors[ ImGuiCol_Button ] = ImVec4( 0.16f, 0.18f, 0.20f, 1.00f );
+	colors[ ImGuiCol_ButtonHovered ] = ImVec4( 0.21f, 0.25f, 0.29f, 1.00f );
+	colors[ ImGuiCol_ButtonActive ] = ImVec4( 0.24f, 0.27f, 0.31f, 1.00f );
+	colors[ ImGuiCol_Header ] = ImVec4( 0.18f, 0.20f, 0.22f, 1.00f );
+	colors[ ImGuiCol_HeaderHovered ] = ImVec4( 0.22f, 0.25f, 0.29f, 1.00f );
+	colors[ ImGuiCol_HeaderActive ] = ImVec4( 0.25f, 0.28f, 0.32f, 1.00f );
+	colors[ ImGuiCol_Separator ] = ImVec4( 0.28f, 0.28f, 0.28f, 1.00f );
+	colors[ ImGuiCol_ResizeGrip ] = ImVec4( 0.21f, 0.49f, 0.76f, 0.25f );
+	colors[ ImGuiCol_ResizeGripHovered ] = ImVec4( 0.21f, 0.49f, 0.76f, 0.67f );
+	colors[ ImGuiCol_ResizeGripActive ] = ImVec4( 0.21f, 0.49f, 0.76f, 0.95f );
+	colors[ ImGuiCol_Tab ] = ImVec4( 0.10f, 0.13f, 0.15f, 1.00f );
+	colors[ ImGuiCol_TabHovered ] = ImVec4( 0.21f, 0.49f, 0.76f, 1.00f );
+	colors[ ImGuiCol_TabActive ] = ImVec4( 0.16f, 0.30f, 0.48f, 1.00f );
+	colors[ ImGuiCol_TabUnfocused ] = ImVec4( 0.12f, 0.12f, 0.13f, 1.0f );
+	colors[ ImGuiCol_TabUnfocusedActive ] = ImVec4( 0.18f, 0.18f, 0.20f, 1.0f );
 
-	// TODO: Adjust colors
+	// Style tweaks
+	style.WindowRounding = 5.0f;
+	style.FrameRounding = 4.0f;
+	style.ScrollbarRounding = 6.0f;
+	style.GrabRounding = 3.0f;
+	style.TabRounding = 4.0f;
+
+	style.WindowPadding = ImVec2( 10, 10 );
+	style.FramePadding = ImVec2( 6, 4 );
+	style.ItemSpacing = ImVec2( 6, 6 );
+	style.ItemInnerSpacing = ImVec2( 4, 4 );
+	style.TouchExtraPadding = ImVec2( 0, 0 );
+	style.ScaleAllSizes( 1.f );
 }
 
 void ColoredLabel( const std::string& label, const ImVec2& size, const ImVec4& color )
